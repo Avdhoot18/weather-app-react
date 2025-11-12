3521fde6692c82226227042e1ac7a958f5b19454
Avdhoot <javdhoot1@gmail.com>
2025-11-12 10:56:38 +0530

    Removed exposed API key and added environment variable

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -8,7 +8,7 @@ export default function SearchBox({updateInfo}){
   let [city, setCity] = useState("");
   let [error, setError] = useState(false);
   const API_URL ="https://api.openweathermap.org/data/2.5/weather";
-  const API_KEY = "c29538f7f830ca2d5bfcd0beae67fa0c";
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
   let getWeatherInfo = async () => {
     try{
